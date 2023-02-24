bd26ec77c6e38a8db3d9d11eb596ae64ac330bd8
Chef5 <nunet@qq.com>
2023-02-24 17:57:09 +0800

    feat: 调整已有校区筛选

--- app/Http/Controllers/RUsersController.php
+++ app/Http/Controllers/RUsersController.php
@@ -331,7 +331,12 @@ class RUsersController extends Controller
      */
     public function getSchools(Request $request){
         try {
-            return returnData(true, '操作成功', RUsers::where('team', '<>', 'system')->select('team')->distinct()->get());
+            return returnData(true, '操作成功', RUsers::where('team', '<>', 'system')
+                ->whereNotIn('team', ['system', ''])
+                ->whereNotNull('team')
+                ->select('team')
+                ->distinct()
+                ->get());
         } catch (\Throwable $th) {
             return returnData(false, $th);
         }
